d8056a6c06ec747348085912755e119efd146226
Troy Martin <troy.martin@fcsamerica.com>
2019-11-26 15:42:46 -0600

    fix login ui

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/services/employee.service';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 import { FormGroup, FormControl, RequiredValidator, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
@@ -9,13 +11,20 @@ import { FormGroup, FormControl, RequiredValidator, Validators } from '@angular/
 })
 export class LoginComponent implements OnInit {
 
+  isUserLoggedIn = new BehaviorSubject(false);
+
   loginForm = new FormGroup({
     empId: new FormControl('', Validators.required)
   });
 
-  constructor(private employeeService: EmployeeService, private authService: AuthenticationService) { }
+  constructor(private employeeService: EmployeeService, private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
+    this.isUserLoggedIn.subscribe((isLoggedIn) => {
+      if (isLoggedIn) {
+        this.router.navigate(['']);
+      }
+    });
   }
 
   login() {
